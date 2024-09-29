92d0cb2a03f00a00965ab9d8c1b27eb1aefabaf1
Kaleb <74982167+Kaleb1583@users.noreply.github.com>
2024-09-29 05:21:47 -0500

    Updated vars.js

--- vars.js
+++ vars.js
@@ -1,25 +1,34 @@
-// if you want to do code something your self you can use these to help
-
+// if you want to do code something yourself you can use these to help
 console.clear();
 
-var username = document.querySelector(".username").children[0].innerText.replaceAll("\n                        ", "").replaceAll("                                            ", "")
+var username = document.querySelector(".username").children[0].innerText.replaceAll("\n                        ", "").replaceAll("                                            ", "");
 
+// Getting server status and player count from the page
 var status = document.querySelectorAll(".statuslabel-label")[0].innerText;
 var playersOnline = parseInt(document.querySelector(".live-status-box-value.js-players").innerText.split('/')[0]);
 
-var ls = lastStatus; // allows you to get things like the server ip without reading html (like the next var below) (var is named lastStatus but instead of just "lastStatus" i put "var ls = 'lastStatus' ")
+// Server status from Hermes and legacy systems
+var status = Hermes.pageStatus.statusData;
+var legacyStatus = Hermes.getInstance().getCurrentStatus().getLegacyStatus();
+var ls = lastStatus; // allows you to get things like the server ip without reading html
 
+// IP, version, software info from the page
 var ip = document.getElementById("ip").innerText;
 var version = document.getElementById("version").innerText;
 var software = document.getElementById("software").innerText;
 
+// Countdown timer info
+var countdownElement = document.querySelector(".server-end-countdown");
+var lastestCountdownValue = countdownElement ? countdownElement.innerText : "No countdown";
+
+// Button control functions
 var startButton = document.getElementById("start");
 function startServer() { startButton.click(); }
 
 var stopButton = document.getElementById("stop");
-function stopServer() { stopButton.click(); } 
+function stopServer() { stopButton.click(); }
 
-var cancelButton = document.getElementById("cancel")
+var cancelButton = document.getElementById("cancel");
 function cancel() { cancelButton.click(); }
 
 var restartButton = document.getElementById("restart");
@@ -28,16 +37,13 @@ function restartServer() { restartButton.click(); }
 var confirmButton = document.getElementById("confirm");
 function confirm() { confirmButton.click(); }
 
+console.log(playersOnline);
 
-var COUNTDOWN = false; // idk what this does exactly
+// Countdown-related variables (not entirely clear what they control but included for potential functionality)
+var COUNTDOWN = false;
 var COUNTDOWN_END = COUNTDOWN ? COUNTDOWN + Math.round(Date.now() / 1000) : false;
-// ^ equals unix time, ive tried setting it to a higher ammount of time so the server wouldnt turn on 
-// but its only cosmetic you can make it say 30 mins until the server turns off but if you go into another tab youll
-// see the actual ammount of time left which would be 0:00 - 10:00 mins (<- normally it starts with around 5-10 mins)
+// COUNTDOWN only seems cosmetic. Changing it doesn't affect actual shutdown time, but it changes how it looks.
 
-
-// these are unknow like COUNTDOWN, dont know what they do but you can make a educated guess by the name
-// and you can try to figure out what is is through console
 var CONSOLE_PERMISSION = true;
 const ADMIN = false;
 const HASJMX = true;
@@ -47,23 +53,27 @@ const REWARDED = true;
 const EXTEND_DEFAULT = 1;
 const EXTEND_ONLY_BELOW_DEFAULT = true;
 
+// Aternos custom alert using something like swal alert
+alert({ title: "Alert Title", text: "This is the alert body text." });
+// You can use HTML custom text like <strong>, <b>, <i> & even elements like <h1>.
 
-// aternos has custom alert using something like swal alert
-alert({ title: "Alert Title", text: "This is the alert body text." })
-// you can use html custom text like strong, bold, italic & even elements like <h1>.
+hideAlert(); /* or */ lastAlert.hide(); // when an alert is made, it's stored as lastAlert
 
-hideAlert(); /* (or/equals ->) */ lastAlert.hide(); // (when an alert is made its stored as lastAlert)
+// Language settings
+LANGUAGE; // Accesses the current language
+LANGUAGE_VARIABLES; 
+var LANG = "en"; // for English
 
+// Ad removal on Aternos
+document.querySelector(".header-center").children[0].remove(); // Remove top center ad
+document.querySelector(".sidebar").remove(); // Remove right sidebar ad
+document.querySelector(".ad-replacement").remove(); // Remove ad above server IP
 
-LANGUAGE
-LANGUAGE_VARIABLES
-LANG // for english it would be var LANG = "en"
+// Function to update server status, possibly showing buttons like Start, Stop, Cancel, etc.
+updateServerStatus("");
 
-
-// ads on aternos
-remove top center ad: document.querySelector(".header-center").children[0].remove();
-remove right sidebar ad: document.querySelector(".sidebar").remove();
-remove ad above server ip: document.querySelector(".ad-replacement").remove();
-
-// all together in one line, note they can detect adblockers so when its runned it will pop up the red screen telling you to disable your ad blocker
-document.querySelector(".header-center").children[0].remove(); document.querySelector(".sidebar").remove(); document.querySelector(".ad-replacement").remove(); 
+// Additional unknown variables that may be useful for further customization
+pendingCancelTimeout;
+queueUpdateInterval;
+lastStatusQueuePercentage;
+pendingCancelTimeout;
